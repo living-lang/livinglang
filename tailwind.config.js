18f57b57fc5268c84ba889f00ac88d3a996e1b17
LivingLang <codyvarga@livinglang.com>
2025-01-13 01:03:31 -0800

    Figuring out tailwind. Started implementing it and setting up my future layout. Want it to go live.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['Merriweather', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
